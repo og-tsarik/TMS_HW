6d2ee3038196661b2e4eca034afcd943c5f1c7d8
og-tsarik <tsarikolya@gmail.com>
2023-12-11 19:08:03 +0300

    Исправлено определение методов в FurnitureShop, BookShop, CompShop

--- hw13/shops/shops.py
+++ hw13/shops/shops.py
@@ -25,13 +25,67 @@ class RealShop(AbstractShop):
 
 
 class FurnitureShop(AbstractShop):
+    def __init__(self):
+        self.products = []
+
+    def is_valid(self, product: Product):
+        if not isinstance(product, Product):
+            raise NonProductError("Тип передаваемого значения должен быть Product")
+        pass
+
+    def add_product(self, product: Product):
+        self.is_valid(product)
+        self.products.append(product)
+
+    def sell_product(self, product: Product):
+        self.is_valid(product)
+        self.products.remove(product)
+
+    def all_product(self) -> list[Product]:
+        return self.products
     pass
 
 class BookShop(AbstractShop):
+    def __init__(self):
+        self.products = []
+
+    def is_valid(self, product: Product):
+        if not isinstance(product, Product):
+            raise NonProductError("Тип передаваемого значения должен быть Product")
+        pass
+
+    def add_product(self, product: Product):
+        self.is_valid(product)
+        self.products.append(product)
+
+    def sell_product(self, product: Product):
+        self.is_valid(product)
+        self.products.remove(product)
+
+    def all_product(self) -> list[Product]:
+        return self.products
     pass
 
 
 class CompShop(AbstractShop):
+    def __init__(self):
+        self.products = []
+
+    def is_valid(self, product: Product):
+        if not isinstance(product, Product):
+            raise NonProductError("Тип передаваемого значения должен быть Product")
+        pass
+
+    def add_product(self, product: Product):
+        self.is_valid(product)
+        self.products.append(product)
+
+    def sell_product(self, product: Product):
+        self.is_valid(product)
+        self.products.remove(product)
+
+    def all_product(self) -> list[Product]:
+        return self.products
     pass
 
 
